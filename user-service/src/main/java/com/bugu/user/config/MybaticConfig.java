ec00eb71d6a6df57a6bbebff77cb6a91aa92ee50
hulilei <hulilei@wetool.com>
2018-06-23 14:15:11 +0800

    add user-service config

---
+++ user-service/src/main/java/com/bugu/user/config/MybaticConfig.java
@@ -0,0 +1,15 @@
+package com.bugu.user.config;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by zhoumingtian on 2018-06-23
+ */
+public class MybaticConfig implements EnvironmentAware {
+
+    @Override
+    public void setEnvironment(Environment environment) {
+
+    }
+}
